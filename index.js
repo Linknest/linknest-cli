a95c86e69f705c05aace557be2152ddfc900eaab
Urban Sanden <hej@urre.me>
2016-10-01 19:31:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const rp = require('request-promise');
+const carrier = require("carrier");
+const clear = require("cli-clear");
+const log = console.log;
+const chalk = require('chalk');
+let collection = [];
+
+clear();
+
+log(chalk.yellow('Enter collection (user/collection): '));
+
+process.stdin.resume();
+
+carrier.carry(process.stdin, function(line) {
+
+  collection.title = String(line).split('/')[1];
+  collection.url = `https://linknest.cc/${line}`;
+
+  var options = {
+      uri: collection.url+'/json/',
+      headers: {
+          'User-Agent': 'Request-Promise'
+      },
+      json: true
+  };
+
+  rp(options)
+  .then(function (collectionLinks) {
+
+    log(chalk.magenta.bold('\nCollection %s has %d links'), collection.title, collectionLinks.length);
+    log(chalk.magenta.bold(collection.url+'\n'));
+
+    Object.keys(collectionLinks).forEach(function(key) {
+      log(chalk.green(key+'. ', collectionLinks[key].title));
+      log(chalk.cyan(collectionLinks[key].linkurl));
+    });
+
+    process.exit();
+
+  })
+
+});
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Linknest cli
+
+Get collection links in your terminal
+
+![](https://cloud.githubusercontent.com/assets/307676/19015891/64e7bc2e-880d-11e6-9b11-081dfe5f2555.png)
+
+[![asciicast](https://asciinema.org/a/55dfskyn50jvxrrngc0g0ub26.png)](https://asciinema.org/a/55dfskyn50jvxrrngc0g0ub26)
+
+### Install
+
+```bash
+npm i -g linknest-cli
+```
+
+### Use
+
+```bash
+linknest-cli
+```
+
+Type user/collection to get all links in collection
